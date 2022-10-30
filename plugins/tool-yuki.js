8299a67d6f2a60df74efd1b77a01e099d77cfad8
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-10-30 09:55:31 +0700

    Update tool-yuki.js

--- plugins/tool-yuki.js
+++ plugins/tool-yuki.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 `.trim()
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak, Yuki Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak, Humanz Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
 }
-handler.customPrefix = /^(Hallo Yuki|Hlo Yuki|hi Yuki|Hai Yuki)$/i
+handler.customPrefix = /^(Hallo Humanz|Hlo Humanz|hi Humanz|Hai Humanz)$/i
 handler.command = new RegExp
 
 handler.owner = false
